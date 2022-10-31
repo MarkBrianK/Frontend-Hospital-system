76970db321230ed0aad5d3cc380b85e682870003
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-10-31 09:38:24 +0300

    Staff/shift component made without crud functionality

--- src/pages/Patient.jsx
+++ src/pages/Patient.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import TableShift from "../components/TableShift";
 
 const Patient = () => {
-  return <div>Patient</div>;
+  const [shifts, setShifts] = useState([]);
+
+  const getShifts = async () => {
+    const res = await fetch("/staffs");
+    const data = await res.json();
+    setShifts(data);
+    console.log(shifts);
+  };
+
+  useEffect(() => {
+    getShifts();
+  }, []);
+
+  return (
+    <>
+      <TableShift shifts={shifts}/>
+
+      <div className="mx-auto mt-4">
+        {/* <TableShift shifts={shifts} /> */}
+
+        {/* <TableShift /> */}
+      </div>
+    </>
+  );
 };
 
 export default Patient;
