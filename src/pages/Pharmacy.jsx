a1eccaa7f0a28a728e343cbfa3c7512c49b1c95b
Anthony Kimeu <tkymeu@gmail.com>
2022-11-01 11:50:27 +0300

    updated pharmacy form

--- src/pages/Pharmacy.jsx
+++ src/pages/Pharmacy.jsx
@@ -1,63 +1,155 @@
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+import { Box, Button, FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
+import React, { useEffect,useState } from 'react'
+import { Container } from '@mui/system';
 
-function Pharmacy() {
+
+const Technicians = ({ handlePosting}) => {
+ 
+
+
+  // const [tech, setTech] = useFetchData()
+  // const deleteTech = ()=>{
+  //   fetch(`http://localhost:9292/technicians/${id}`, {
+  //     method: "DELETE",
+  //     headers: {
+  //       "Content-Type":"application/json"
+  //     }
+  //   })
+  //   .then(res=>res.json())
+  //   .then(data => console.log(data))
+  //   .catch(err=>console.log(err))
+
+  //   fetch("http://localhost:9292/technicians")
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setTech(data)
+  //       console.log(data)
+  //     })
+  //     .catch(err => console.log(err))
+  // }
+
+
+
+    const [formData, setFormData] = useState({
+      patient_id: '',
+      ticket_id: '',
+      Remark: '',
+      inventory_item: '',
+      town: '',
+      job_type: '',
+      job_description: '',
+      labour_cost: ''
+
+    })
+
+
+      
+    const [technicians, setTechnicians] = useState([])
+    useEffect( () => {
+      fetch("https://backend-host.herokuapp.com/technicians")
+      .then(res => res.json())
+      .then(data => setTechnicians(data))
+    },[])
+
+    function handleSubmit(e){
+        e.preventDefault();
+        fetch(`https://backend-host.herokuapp.com/technicians`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(r => r.json())
+        .then(data => {
+            handlePosting(data)
+        })
+
+        setFormData({
+          patient_id: '',
+          ticket_id: '',
+          Remark: '',
+          inventory_item: '',
+          town: '',
+          job_type: '',
+          job_description: '',
+          labour_cost: ''
+        })
+    }
+
+    function handleChange(e){
+        setFormData({
+            ...formData, [e.target.name]: e.target.value,
+        });
+    }  
+
   return (
-    <Form style={{width: "100%"}}>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Patient Name</Form.Label>
-          <Form.Control type="patient name" placeholder="Enter Patient Name" />
-        </Form.Group>
+    <div>
+      <div>
+        <Container className='formContainer'>
+    <div 
+    style={{ fontSize: "20px", fontWeight: "bold" }}
+    >
+        Pharmacy Form 
+    </div>
+    <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+      
+      <div>
+        <FormControl sx={{ m: 1, width: '35ch' }}>
+            <InputLabel>Patient Id</InputLabel>
+            <Input name="patient_id" value={formData.patient_id} onChange={handleChange}/>
+            <FormHelperText>Please Enter Patient id</FormHelperText>
+        </FormControl>
+      </div>
 
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Inventory Item</Form.Label>
-          <Form.Control type="inventory item" placeholder="Inventory Item" />
-        </Form.Group>
-      </Row>
+      <div>
+        <FormControl sx={{ m: 2, width: '35ch' }}>
+            <InputLabel>Ticket id</InputLabel>
+            <Input name="ticket_id" value={formData.ticket_id} onChange={handleChange}/>
+            <FormHelperText>Please Enter Ticket Id</FormHelperText>
+        </FormControl>
+      </div>
 
-      <Form.Group className="mb-" controlId="formGridAddress1">
-        <Form.Label>Amount</Form.Label>
-        <Form.Control placeholder="Amount" />
-      </Form.Group>
+      <div>
+        <FormControl sx={{ m: 1, width: '35ch' }}>
+            <InputLabel>Remarks</InputLabel>
+            <Input name="Remark" value={formData.Remark} onChange={handleChange}/>
+            <FormHelperText>Please Enter Remarks</FormHelperText>
+        </FormControl>
+      </div>
 
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Remarks</Form.Label>
-        <Form.Control placeholder="Remarks" />
-      </Form.Group>
+      <div>
+        <FormControl sx={{ m: 1, width: '35ch' }}>
+            <InputLabel>Inventory item</InputLabel>
+            <Input name="inventory_item" value={formData.inventory_item} onChange={handleChange}/>
+            <FormHelperText>Please Enter Inventory item</FormHelperText>
+        </FormControl>
+      </div>
 
-      <Row className="mb-4">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>Ticket No</Form.Label>
-          <Form.Control />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>Mrp</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            {/* <option>Choose...</option>
-            <option>...</option> */}
-          </Form.Select>
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Discount</Form.Label>
-          <Form.Control />
-        </Form.Group>
-      </Row>
-
-      {/* <Form.Group className="mb-4" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group> */}
-
-      <Button variant="primary" type="submit">
-        Make Payment
-      </Button>
-    </Form>
+      <div>
+      </div>
+      <div>
+      </div>
+      
+      
+      <div>
+      </div>
+      
+      <div>
+        
+      </div>
+    </Box>
+    <div>
+        <FormControl sx={{ display: "flex", flexWrap: "wrap", m: 1, width: '10ch' }}>
+            <Button variant='outlined' type='submit' onClick={handleSubmit}>
+                Submit
+            </Button>
+        </FormControl>
+      </div>
+    </Container>
+    </div>
+    </div>
   );
 }
 
-
-export default Pharmacy;
+export default Technicians;
