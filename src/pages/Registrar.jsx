16cd44c065fe60f39a927a53f8ad04e2d34ac247
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-11-03 12:58:50 +0300

    pushing to my branch

--- src/pages/Registrar.jsx
+++ src/pages/Registrar.jsx
@@ -42,8 +42,10 @@ const handleChange = (event) => {
   return(
 <>
 {/* <body id="signup"> */}
+
   <main className="container">
-    <div className="back"></div>
+    
+    <div className="back"></div> 
     <div className="brand">
       <div className="logo">
         <img height="64" src="https://thumbs.dreamstime.com/b/caduceus-medical-symbol-isolated-white-background-d-render-caduceus-medical-symbol-isolated-145721309.jpg" alt="Panda Logo" />
@@ -51,65 +53,67 @@ const handleChange = (event) => {
           <span className="name"><span>Touching</span><span>Lives</span></span>Foundation
         </h1>
       </div>
-      <span className="copyright">Photo by
+      {/* <span className="copyright">Photo by
         <a href="https://unsplash.com/@filipz" target="_blank" rel="noreferrer" title="Photographer">Filip Zrnzević</a>
         on
-        <a href="https://unsplash.com/photos/QsWG0kjPQRY" target="_blank" rel="noreferrer"title="Background Photo">Unsplash</a></span>
+        <a href="https://unsplash.com/photos/QsWG0kjPQRY" target="_blank" rel="noreferrer"title="Background Photo">Unsplash</a></span> */}
     </div>
+    
     <div className="formWrapper">
       <div className="form">
         <h2>Silver Crest Hospital</h2>
-        <form id="form" method="get" action="#">
+        <form onSubmit={handlePostSubmit} id="form" method="get" action="#">
           <div className="inputWrapper">
-            <input type="text" name="first" id="first" required />
-            <label for="first">Ticket Number</label>
+            <input type="text"name="ticket_no" id="first" onChange={handleChange}required />
+            <label htmlFor="first">Ticket Number</label>
           </div>
           <div className="inputWrapper">
-            <input type="text" name="last" id="last" required />
-            <label for="last">Patient Name</label>
+            <input type="text" name="patient_no" id="last" onChange={handleChange} required />
+            <label htmlFor="last">Patient Name</label>
           </div>
           <div className="inputWrapper">
-            <input type="reg_date" name="reg_date" id="reg_date" required />
-            <label for="reg_date">Registration Date</label>
+            <input type="reg_date" name="reg_date" id="reg_date" required onChange={handleChange}/>
+            <label htmlFor="reg_date">Registration Date</label>
           </div>
           <div className="inputWrapper">
-            <input type="Address" name="Address" id="Address" required />
-            <label for="Address">Address</label>
+            <input type="Address" name="address" id="Address" onChange={handleChange} required />
+            <label htmlFor="Address">Address</label>
           </div>
           <div className="inputWrapper">
-            <input type="dob" name="dob" id="dob" required />
-            <label for="dob">Date of Birth</label>
+            <input type="dob" name="dob" id="dob"onChange={handleChange} required />
+            <label htmlFor="dob">Date of Birth</label>
           </div>
           <div className="inputWrapper">
-            <input type="contact_no" name="contact_no" id="contact_no" required />
-            <label for="contact_no">Phone Number</label>
+            <input type="contact_no" name="contact_no" id="contact_no" onChange={handleChange}required />
+            <label htmlFor="contact_no">Phone Number</label>
           </div>
           <div className="inputWrapper">
-            <input type="email" name="email" id="email" required />
-            <label for="email">Email</label>
+            <input type="email" name="email" id="email" onChange={handleChange}required />
+            <label htmlFor="email">Email</label>
           </div>
           <div className="inputWrapper">
-            <input type="tel" name="guardian" id="guardian" required />
-            <label for="guardian">Guardian Contact</label>
+            <input type="tel" name="guardian" id="guardian" onChange={handleChange}required />
+            <label htmlFor="guardian">Guardian Contact</label>
           </div>
           <div className="inputWrapper">
-            <input type="relation" name="relation" id="relation" required />
-            <label for="relation">Guardian Relation</label>
+            <input type="relation" name="relation" id="relation" onChange={handleChange}required />
+            <label htmlFor="relation">Guardian Relation</label>
           </div>
           <div className="inputWrapper">
-            <input type="gender" name="gender" id="gender" required />
-            <label for="gender"> gender</label>
+            <input type="gender" name="gender" id="gender" onChange={handleChange}required />
+            <label htmlFor="gender"> gender</label>
           </div>
           <div className="inputWrapper">
-            <input type="patient_status" name="patient_status" id="patient_status" required />
-            <label for="patient_status"> patient_status</label>
+            <input type="patient_status" name="patient_status" id="patient_status" onChange={handleChange}required />
+            <label htmlFor="patient_status"> patient_status</label>
           </div>
         </form>
         <input form="form" type="submit" name="register" id="register" value="REGISTER" />
+     
         
       </div>
     </div>
-  </main>
+    </main>
 {/* </body> */}
 </>
   )
